9938cda4c47ade661f04e3a3f085962154b46444
Given22 <71041120+Given22@users.noreply.github.com>
2022-05-23 19:36:54 +0200

    Add Leaflet Map Library

--- script.js
+++ script.js
@@ -11,12 +11,23 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     (position) => {
-      const {latitude} = position.coords;
-      const {longitude} = position.coords;
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
       console.log(position, latitude, longitude);
+
+      const coords = [latitude, longitude];
+
+      const map = L.map("map").setView(coords, 15);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+      L.marker(coords).addTo(map).bindPopup("Your location").openPopup();
     },
     (error) => {
       alert(error.message);
