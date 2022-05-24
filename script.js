74ec14871f56540645a1770cae2cf7aa9004d10d
Given22 <71041120+Given22@users.noreply.github.com>
2022-05-24 17:28:17 +0200

    Add class for workouts

--- script.js
+++ script.js
@@ -11,6 +11,41 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+class Workout {
+  date = new Date();
+  #id = (Date.now() + "").slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+  }
+}
+
 class App {
   #map;
   #mapE;
@@ -18,9 +53,9 @@ class App {
 
   constructor() {
     this._getPosition();
-    
+
     form.addEventListener("submit", this._newWorkout.bind(this));
-    
+
     inputType.addEventListener("change", this._toggleElevationField);
   }
 
@@ -52,7 +87,7 @@ class App {
   }
 
   _showForm(e) {
-    this.#mapE = e
+    this.#mapE = e;
     form.classList.remove("hidden");
     inputDistance.focus();
   }
@@ -80,4 +115,4 @@ class App {
 
 const mapty = new App();
 
-
+const run1 = new Running()
