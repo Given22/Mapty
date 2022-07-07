93fe02914118205793c78bae170f242e8a5f0617
Damian S <71041120+Given22@users.noreply.github.com>
2022-07-07 14:19:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -595,21 +595,3 @@ class App {
 }
 
 const mapty = new App();
-
-// Ability to edit a workout ✅
-
-// Ability to delete a workout ✅
-
-// Ability to delete all workouts ✅
-
-// Ability to sort workouts byacertain field (e.g. distance) ✅
-
-// Re-build Running and Cycling objects coming from Local Storage ✅
-
-// Ability to position the map to show all workouts[very hard] ✅
-
-// Ability to draw lines and shapes instead of just points[very hard]
-
-// Geocode location from coordinates("Run in Faro,Portugal")[only after asynchronous JavaScript section]
-
-// Display weather data for workout time and place[only after asynchronous JavaScript section]
